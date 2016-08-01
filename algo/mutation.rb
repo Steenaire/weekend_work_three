e5d772d98405b841951c5158c37b4cc5b751d716
Steen Clarke <cmclarkey@gmail.com>
2016-08-01 09:20:56 -0700

    Finish mutation assignment

--- algo/mutation.rb
+++ algo/mutation.rb
@@ -3,6 +3,22 @@
 # the first string, and false otherwise.
 
 def mutation?(base_word, mutation)
+  letter_counter = 0
+  mut_array = mutation.split("")
+  base_array = base_word.split("")
+
+  mut_array.each do |mut_letter|
+    if base_array.include? mut_letter
+      letter_counter += 1
+      base_array -= [mut_letter] #Makes sure the letter doesn't get counted twice
+    end
+  end
+
+  if letter_counter == mutation.length
+    return true
+  else
+    return false
+  end
 
 end
 
