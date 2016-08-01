ca291cfe4890e0b7acd1c2b60e7013f3a93bda7a
Steen Clarke <cmclarkey@gmail.com>
2016-08-01 09:35:39 -0700

    Finish hotel_reservation exercise

--- oop/hotel_reservation.rb
+++ oop/hotel_reservation.rb
@@ -32,3 +32,27 @@ end
 # The add_a_crib method
 # The add_a_custom_amenity method
 
+require 'date'
+
+reservation1 = HotelReservation.new({
+  customer_name: "Steen",
+  date: Date.today,
+  room_number: "5A",
+  amenities: []
+  })
+
+puts "Original room number: #{reservation1.room_number}"
+reservation1.room_number = "6A"
+puts "Room number after change: #{reservation1.room_number}"
+
+puts "Amenities before: #{reservation1.amenities}"
+reservation1.add_a_fridge
+puts "Amenities after add_a_fridge: #{reservation1.amenities}"
+
+puts "Amenities before: #{reservation1.amenities}"
+reservation1.add_a_crib
+puts "Amenities after add_a_crib: #{reservation1.amenities}"
+
+puts "Amenities before: #{reservation1.amenities}"
+reservation1.add_a_custom_amenity("hot tub")
+puts "Amenities after add_a_custom_amenity: #{reservation1.amenities}"
